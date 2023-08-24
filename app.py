84c2bd61560ce727c080d07e4ffe19bd255ba222
MrSivakumarraju <sivakumarraju.skr@gmail.com>
2023-08-24 22:12:00 +0530

    added app.py and functions

---
+++ app.py
@@ -0,0 +1,72 @@
+import streamlit as st 
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap  = 200,
+        length_function = len
+        )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+def get_vectorstore(text_chunks):
+    embeddings = OpenAIEmbeddings()
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
+
+def get_converstion_chain(vectorstore):
+    llm = 
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from llm
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
+
+    st.header("Chat with multiple PDFs :books:")
+    st.text_input("Ask a Question about your documents:")
+
+    with st.sidebar:
+        st.subheader("Your documents")
+        pdf_docs = st.file_uploader(
+            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing..."):
+                # get pdf text 
+                raw_text = get_pdf_text(pdf_docs)
+
+                #get text chunks 
+                text_chunks = get_text_chunks(raw_text)
+                #st.write(text_chunks)
+
+                #create vettor store 
+                vectorstore = get_vectorstore(text_chunks)
+
+                # create converstion chain
+                converstion = get_converstion_chain(vectorstore)
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
